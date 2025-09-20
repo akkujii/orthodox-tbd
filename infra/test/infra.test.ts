e4d2c5750ae1385ff41907cb993d48ab89c3a3cd
akkuJii <peinola+gh@gmail.com>
2025-09-20 19:53:19 +0300

    ci: configure linter & formatter

--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,17 +1,17 @@
-import { describe, test } from 'vitest';
-import { Template } from 'aws-cdk-lib/assertions';
-import * as cdk from 'aws-cdk-lib';
-import * as Infra from '../lib/infra-stack';
+import { describe, test } from "vitest";
+import { Template } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import * as Infra from "../lib/infra-stack";
 
-describe('InfraStack', () => {
-	test('SQS Queue Created', () => {
-		const app = new cdk.App();
-		// WHEN
-		const stack = new Infra.InfraStack(app, 'MyTestStack');
-		// THEN
-		const template = Template.fromStack(stack);
-		template.hasResourceProperties('AWS::SQS::Queue', {
-			VisibilityTimeout: 300
-		});
-	});
+describe("InfraStack", () => {
+  test("SQS Queue Created", () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Infra.InfraStack(app, "MyTestStack");
+    // THEN
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::SQS::Queue", {
+      VisibilityTimeout: 300,
+    });
+  });
 });
