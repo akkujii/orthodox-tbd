f9ebbdcbf2283f7fe0e24aa30f8c465e1b50ab66
akkuJii <peinola+gh@gmail.com>
2025-09-20 21:46:19 +0300

    test: add snapshot test

--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,17 +1,12 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { Template } from "aws-cdk-lib/assertions";
 import * as cdk from "aws-cdk-lib";
 import * as Infra from "../lib/infra-stack";
 
 describe("InfraStack", () => {
-  test("SQS Queue Created", () => {
+  test("should match snapshot", () => {
     const app = new cdk.App();
-    // WHEN
     const stack = new Infra.InfraStack(app, "MyTestStack");
-    // THEN
-    const template = Template.fromStack(stack);
-    template.hasResourceProperties("AWS::SQS::Queue", {
-      VisibilityTimeout: 300,
-    });
+    expect(Template.fromStack(stack)).toMatchSnapshot();
   });
 });
